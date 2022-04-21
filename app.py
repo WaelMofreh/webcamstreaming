62c8c25880371b4459ce22b7a828baad889b182a
WaelMofreh <wael.elsharkawy.9@gmail.com>
2022-04-21 02:21:55 +0200

    Type in a message

--- app.py
+++ app.py
@@ -1,13 +1,2 @@
-from streamlit_webrtc import webrtc_streamer, RTCConfiguration
-import av
-import cv2
-
-class VideoProcessor:
-	def recv(self, frame):
-		frm = frame.to_ndarray(format="bgr24")
-
-webrtc_streamer(key="key", video_processor_factory=VideoProcessor,
-				rtc_configuration=RTCConfiguration(
-					{"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
-					)
-	)
+from streamlit_webrtc import webrtc_streamer
+webrtc_streamer(key="key")
