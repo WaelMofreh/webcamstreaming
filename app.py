7bc3e24106be6c14f529c1759ec1e5e2d35aa52b
WaelMofreh <wael.elsharkawy.9@gmail.com>
2022-04-21 02:15:51 +0200

    Add project files.

---
+++ app.py
@@ -0,0 +1,13 @@
+from streamlit_webrtc import webrtc_streamer, RTCConfiguration
+import av
+import cv2
+
+class VideoProcessor:
+	def recv(self, frame):
+		frm = frame.to_ndarray(format="bgr24")
+
+webrtc_streamer(key="key", video_processor_factory=VideoProcessor,
+				rtc_configuration=RTCConfiguration(
+					{"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
+					)
+	)
